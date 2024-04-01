9ebe115b98cea5efc3970f91e9da060d7c55e9a3
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2024-04-01 16:26:40 -0400

    Initial structure of app

---
+++ JustWeather/JustWeather/App/JustWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustWeatherApp.swift
+//  JustWeather
+//
+//  Created by Jeffrey Sweeney on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct JustWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
